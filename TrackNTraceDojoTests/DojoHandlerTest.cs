a4236998ceb5efc398d3e583810e87292167748e
Aguiar <tomas.h.aguiar@accenture.com>
2022-02-08 17:41:41 -0300

    first commit

---
+++ TrackNTraceDojoTests/DojoHandlerTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TrackNTraceDojo;
+using Xunit;
+
+namespace TrackNTraceDojoTests
+{
+    public class DojoHandlerTest
+    {
+        private readonly DojoHandler _dojoHandlerInstance;
+
+        public DojoHandlerTest()
+        {
+            _dojoHandlerInstance = new DojoHandler();
+        }
+
+        [Fact]
+        public void GetListOfRandomNames()
+        {
+            var names = new List<string>
+            {
+                "name1",
+                "name2",
+                "name3"
+            };
+            var namesList = Sort.Randomize(names);
+
+            Assert.NotEqual(names, namesList);
+        }
+
+        [Fact]
+        public void GetRotationTime()
+        {
+            var dojoHandlerInstance = new DojoHandler();
+            dojoHandlerInstance.SetRotationTime(5);
+            var time = dojoHandlerInstance.GetRotationTime();
+            Assert.True(time == 5);
+        }
+
+        [Fact]
+        public void GetCommitTime()
+        {
+            var dojoHandlerInstance = new DojoHandler();
+            dojoHandlerInstance.SetCommitTime(2);
+            var time = dojoHandlerInstance.GetCommitTime();
+            Assert.True(time == 2);
+        }
+
+        [Fact]
+        public void SetNamesForRotation()
+        {
+            var names = new List<string>
+            {
+                "name1",
+                "name2",
+                "name3"
+            };
+
+            _dojoHandlerInstance.SetRotationNames(names);
+            var rotationList = _dojoHandlerInstance.GetNames();
+
+            Assert.Equal(names, rotationList);
+            Assert.Equal(names.Count, rotationList.Count);
+        }
+
+        [Fact]
+        public void CreateDojoRound()
+        {
+            const string navigator = "some-one";
+            const string driver = "some-driver";
+            var start = DateTime.Now;
+            const int rotationTime = 5;
+            var end = start.AddMinutes(rotationTime);
+
+            _dojoHandlerInstance.CreateRound(driver, navigator, start, end);
+
+            var recordList = _dojoHandlerInstance.GetRounds();
+
+            Assert.Equal(navigator, recordList.First().Navigator);
+        }
+
+
+        [Fact]
+        public void CreateDojo()
+        {
+            List<string> names = new List<string>();
+            names.Add("Lucas");
+            names.Add("Saulo");
+            const int rotationTime = 5;
+            const int commitTime = 2;
+
+            _dojoHandlerInstance.CreateDojo(names, rotationTime, commitTime);
+
+
+
+
+
+        }
+    }
+}
---
+++ trackntracedojocore/DojoHandler.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrackNTraceDojo
+{
+    public class DojoHandler
+    {
+        private int RotationTime { get; set; }
+        private int CommitTime { get; set; }
+        private List<string> Names { get; set; }
+
+        public List<string> GetNames() => Names;
+
+        private List<DojoRound> DojoRounds { get; set; }
+
+        public DojoHandler()
+        {
+            DojoRounds = new List<DojoRound>();
+        }
+
+        public void SetRotationTime(int minutes)
+        {
+            RotationTime = minutes;
+        }
+        public void SetCommitTime(int minutes)
+        {
+            CommitTime = minutes;
+        }
+
+        public int GetRotationTime()
+        {
+            return RotationTime;
+        }
+        
+        public int GetCommitTime()
+        {
+            return CommitTime;
+        }
+
+        public void SetRotationNames(List<string> names)
+        {
+            Names = names;
+        }
+
+
+
+        public void CreateRound(string driver, string navigator, DateTime start, DateTime end)
+        {
+            DojoRounds.Add(new DojoRound()
+            {
+                Driver = driver,
+                Navigator = navigator,
+                Start = start,
+                End = end
+            });
+        }
+
+
+        public List<DojoRound> GetRounds()
+        {
+            return DojoRounds;
+        }
+
+        public void CreateDojo(List<string> names, int rotationTimeInMinutes, int commitTimeInMinutes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DojoRound
+    {
+        public string Driver { get; set; }
+        public string Navigator { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+
+    public class RandomNames
+    {
+        public Guid Id;
+        public string Name;
+    }
+
+    public static class Sort
+    {
+        public static List<string> Randomize(List<string> namesList)
+        {
+            List<string> listNamesOrdered;
+            do
+            {
+                var names = new List<RandomNames>();
+                listNamesOrdered = namesList.OrderBy(i => Guid.NewGuid()).Select(x => x).ToList();
+            } while (listNamesOrdered.Equals(namesList));
+            
+            return listNamesOrdered;
+        }
+    }
+    
+    
+}
